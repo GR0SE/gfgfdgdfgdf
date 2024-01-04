b268b20420621ae4df66274192ba6f5342321af2
GR0SE <133581093+GR0SE@users.noreply.github.com>
2024-01-04 19:39:03 +0200

    1

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+document.getElementById("open-automation").onclick = function () {
+    let automation = document.getElementById("automation");
+    automation.classList.toggle("container-information-automation-activate");
+};
