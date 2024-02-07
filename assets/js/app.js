fb8fc8db1f59139bd2771c7e611a0e8336fae524
GR0SE <133581093+GR0SE@users.noreply.github.com>
2024-02-07 22:58:52 +0200

    1

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+    ScrollSmoother.create({
+        wrapper: ".wrapper",
+        content: ".content",
+        smooth: 1.5,
+        effects: true,
+    });
+});
--- assets/js/index.js
+++ assets/js/index.js
@@ -28,41 +28,43 @@ document.getElementById("open-automation").onclick = function () {
 //     }
 // });
 
-function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <=
-            (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <=
-            (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
+// !!!importent
+// function isElementInViewport(el) {
+//     var rect = el.getBoundingClientRect();
+//     return (
+//         rect.top >= 0 &&
+//         rect.left >= 0 &&
+//         rect.bottom <=
+//             (window.innerHeight || document.documentElement.clientHeight) &&
+//         rect.right <=
+//             (window.innerWidth || document.documentElement.clientWidth)
+//     );
+// }
 
-var products = document.querySelector(".products");
-var contact = document.querySelector(".contact-us-wrapper");
-var productsAnimated = false;
-var contactAnimated = false;
+// var products = document.querySelector(".products");
+// var contact = document.querySelector(".contact-us-wrapper");
+// var productsAnimated = false;
+// var contactAnimated = false;
 
-window.addEventListener("scroll", function () {
-    if (!productsAnimated && isElementInViewport(products)) {
-        products.classList.remove("hidden");
-        products.classList.add("animate-left");
-        productsAnimated = true;
-    }
-    if (contact && !contactAnimated && isElementInViewport(contact)) {
-        contact.classList.remove("hidden");
-        contact.classList.add("animate-down");
-        contactAnimated = true;
-    }
-});
-document.addEventListener("DOMContentLoaded", function () {
-    if (products) products.classList.add("hidden");
-    if (contact) contact.classList.add("hidden");
-});
-
+// window.addEventListener("scroll", function () {
+//     if (!productsAnimated && isElementInViewport(products)) {
+//         products.classList.remove("hidden");
+//         products.classList.add("animate-left");
+//         productsAnimated = true;
+//     }
+//     if (contact && !contactAnimated && isElementInViewport(contact)) {
+//         contact.classList.remove("hidden");
+//         contact.classList.add("animate-down");
+//         contactAnimated = true;
+//     }
+// });
+// document.addEventListener("DOMContentLoaded", function () {
+//     if (products) products.classList.add("hidden");
+//     if (contact) contact.classList.add("hidden");
+// });
+// !!!importent
 // !!
+
 // var mySwiper = new Swiper(".swiper-container", {
 //     // Параметры Swiper
 //     loop: true,
@@ -97,3 +99,5 @@ const sliderMain = new Swiper(".slider_main", {
     // },
     // effect: fade,
 });
+
+// плавный скрол!
